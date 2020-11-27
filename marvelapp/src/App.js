a95d90de78b07929a4f55411f0ec7e9c9c58ec1d
n.hernandezs <n.hernandezs@uniandes.edu.co>
2020-11-27 17:39:49 -0500

    .

--- marvelapp/src/App.js
+++ marvelapp/src/App.js
@@ -1,26 +1,11 @@
-import React, {useState,useEffect} from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Personaje from './Personaje.js';
 
 function App() {
-  const [personaje, setPersonaje] = useState(null);
-
-  useEffect(()=>{
-    if(!navigator.onLine){
-        if(localStorage.getItem("joke") === null) {
-          setPersonaje("Loading...")
-        } else {
-          setPersonaje(localStorage.getItem("joke"));
-        }
-    } else {
-        const URL = "https://api.chucknorris.io/jokes/random";
-        fetch(URL).then(res=>res.json()).then(res=>{
-            setPersonaje(res.value);
-            localStorage.setItem("joke", res.value);
-        })
-    }
-}, []);
+ 
+    
   return (
     <div className="App">
       <header className="App-header">
--- marvelapp/src/Personaje.js
+++ marvelapp/src/Personaje.js
@@ -1,6 +1,28 @@
 import React, { useState, useEffect } from "react";
-export default function Personaje(){
-    return(<>
-    <h1>Personajes de marvel</h1>
-    </>)
+export default function Personaje() {
+  const [personaje, setPersonaje] = useState(null);
+
+  useEffect(() => {
+    if (!navigator.onLine) {
+      if (localStorage.getItem("joke") === null) {
+        setPersonaje("Loading...");
+      } else {
+        setPersonaje(localStorage.getItem("joke"));
+      }
+    } else {
+      const URL = "https://api.chucknorris.io/jokes/random";
+      fetch(URL)
+        .then((res) => res.json())
+        .then((res) => {
+          setPersonaje(res.value);
+          localStorage.setItem("joke", res.value);
+        });
+    }
+  }, []);
+  return (
+    <>
+      <h1>Personajes de marvel</h1>
+      {personaje}
+    </>
+  );
 }
